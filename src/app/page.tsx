69b0d7ea0757e6c8bdbd543137747c5079b3de03
Ivan <kyurkchyu10@gmail.com>
2024-03-06 17:47:22 +0200

    updating form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,15 @@
 'use client'
-
-import { useForm, SubmitHandler } from "react-hook-form"
-import {Button, Container, Stack, TextField} from "@mui/material";
-
-type Inputs = {
-    url: string
-    firstName: string
-    age: number
-}
+import Header from "@/components/header/Header"
+import Form from "@/components/form/Form"
+import SideBar from "@/components/side-bar/SideBar";
 
 export default function App() {
 
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm<Inputs>()
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
-
-    console.log(watch("url")) // watch input value by passing the name of it
-
     return (
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-        <Container maxWidth="sm">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                {/* register your input into the hook by invoking the "register" function */}
-                <Stack spacing={2}>
-                    <TextField
-                        defaultValue="https://localhost:3000"
-                        {...register("url")}
-                        placeholder="Enter url"
-                    />
-                    <TextField
-                        {...register("firstName", {required: true})}
-                        placeholder="Enter First Name"
-                    />
-                    {errors.firstName && <span>This field is required</span>}
-                    {/* include validation with required or other standard HTML validation rules */}
-                    <TextField
-                        type="number"
-                        {...register("age", {required: true})}
-                        placeholder="Enter age"
-                    />
-                    <input type="checkbox" />
-                    {/* errors will return when field validation fails  */}
-                    {errors.age && <span>This field is required</span>}
-                    <Button type="submit" variant="contained">Submit</Button>
-                </Stack>
-            </form>
-        </Container>
+        <>
+            <Header />
+            {/*<SideBar />*/}
+            <Form />
+        </>
     )
 }
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,11 +1,189 @@
 import React from 'react';
+import { useForm, SubmitHandler } from "react-hook-form"
+import {Button, Container, Stack, TextField} from "@mui/material";
+import styled from "@emotion/styled";
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import Typography from "@mui/material/Typography";
 
-const Form = () => {
+type Inputs = {
+    url: string
+    jwt_secret_ttl: number
+    refresh_secret_ttl: number
+    email_host: string
+    email_port: number
+    enable_google_oauth: boolean
+    oauth_google_client_id: string
+    oauth_google_client_secret: string
+    enable_ldap: boolean
+    ldap_url: string
+    ldap_bind_dn: string
+}
+
+const schema = z.object({
+    url: z.string(),
+    jwt_secret_ttl: z.number(),
+    refresh_secret_ttl: z.number().min(0, "Min value is 0").max(10, "Max value is 10"),
+    email_host: z.string(),
+    email_port: z.number(),
+    enable_google_oauth: z.boolean(),
+    oauth_google_client_id: z.string(),
+    oauth_google_client_secret: z.string(),
+    enable_ldap: z.boolean(),
+    ldap_url: z.string(),
+    ldap_bind_dn: z.string(),
+})
+
+export default function Form() {
+
+    const form = useForm<Inputs>({
+        defaultValues: {
+            url: "https://localhost:3000",
+            jwt_secret_ttl: 0,
+            refresh_secret_ttl: 0,
+            email_host: "",
+            email_port: 0,
+            enable_google_oauth: false,
+            oauth_google_client_id: "",
+            oauth_google_client_secret: "",
+            enable_ldap: false,
+            ldap_url: "",
+            ldap_bind_dn: ""
+        },
+        resolver: zodResolver(schema)
+    })
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        reset,
+        formState: { errors },
+    } = form
+
+    const CustomForm = styled.form`
+        padding: 20px 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+    `
+    const enableGoogleOauth = watch("enable_google_oauth")
+    const enableLdap = watch("enable_ldap")
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+
     return (
-        <div>
+        <Container>
+            <h1>Some form</h1>
+            <CustomForm onSubmit={handleSubmit(onSubmit)}>
+                <Stack spacing={4}>
+                    <Stack direction="row">
+                        <TextField
+                            {...register("url")}
+                            label="Application URL:"
+                        />
+                    </Stack>
+                    <Typography>
+                        Authentication Security Settings:
+                    </Typography>
+                    <Stack direction="row" spacing={2}>
+                        <TextField
+                            {...register("jwt_secret_ttl")}
+                            error={!!errors.jwt_secret_ttl}
+                            helperText={errors.jwt_secret_ttl?.message}
+                            label="JWT Secret TTL:"
+                        />
+                        <TextField
+                            type="number"
+                            {...register("refresh_secret_ttl", {
+                                required: "Refresh Secret TTL is required"
+                            })}
+                            error={!!errors.refresh_secret_ttl}
+                            helperText={errors.refresh_secret_ttl?.message}
+                            label="Refresh Secret TTL:"
+                        />
+                    </Stack>
 
-        </div>
-    );
-};
+                    <Typography>
+                        Email Settings:
+                    </Typography>
 
-export default Form;
+                    <Stack direction="row" spacing={2}>
+                        <TextField
+                            {...register("email_host")}
+                            error={!!errors.email_host}
+                            helperText={errors.email_host?.message}
+                            label="Email Host:"
+                        />
+                        <TextField
+                            type="number"
+                            {...register("email_port", {
+                                required: "Refresh Secret TTL is required"
+                            })}
+                            error={!!errors.email_port}
+                            helperText={errors.email_port?.message}
+                            label="Email Port:"
+                        />
+                    </Stack>
+
+                    <Typography>
+                        Google Settings:
+                    </Typography>
+
+                    <Stack direction="row">
+                        <input
+                            type="checkbox"
+                            {...register("enable_google_oauth")}
+                        />
+                    </Stack>
+                    <Stack direction="row" spacing={2}>
+                        <TextField
+                            {...register("oauth_google_client_id", {
+                                disabled: !enableGoogleOauth
+                            })}
+                            label="OAUTH Google Client ID:"
+                        />
+
+                        <TextField
+                            {...register("oauth_google_client_secret", {
+                                disabled: !enableGoogleOauth
+                            })}
+                            label="OAUTH Google Client Secret:"
+                        />
+                    </Stack>
+
+                    <Typography>
+                        LDAP Settings:
+                    </Typography>
+
+                    <Stack direction="row">
+                        <input
+                            type="checkbox"
+                            {...register("enable_ldap")}
+                        />
+                    </Stack>
+                    <Stack direction="row" spacing={2}>
+                        <TextField
+                            {...register("ldap_url", {
+                                disabled: !enableLdap
+                            })}
+                            label="LDAP URL:"
+                        />
+
+                        <TextField
+                            {...register("ldap_bind_dn", {
+                                disabled: !enableLdap
+                            })}
+                            label="LDAP BIND DN:"
+                        />
+                    </Stack>
+
+                    <Stack direction="row" spacing={2} justifyContent="flex-end">
+                        <Button type="submit" variant="contained">Submit</Button>
+                        <Button type="reset" variant="outlined" onClick={() => reset()}>Reset</Button>
+                    </Stack>
+                </Stack>
+            </CustomForm>
+        </Container>
+    )
+}
